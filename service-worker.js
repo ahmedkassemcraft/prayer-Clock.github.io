c222e917223d8388bd5b274eae0c9aa7921d57bc
ahmedkassemcraft <74984657+ahmedkassemcraft@users.noreply.github.com>
2023-07-02 14:14:20 -0500

    Update service-worker.js pwa 3

--- service-worker.js
+++ service-worker.js
@@ -15,26 +15,9 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // Intercepta las solicitudes de descarga de la PWA
-  if (event.request.url.endsWith('/download')) {
-    event.respondWith(handleDownloadRequest(event.request));
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
-
-async function handleDownloadRequest(request) {
-  // Obtiene el archivo de la PWA para descargar
-  const response = await fetch('/path-to-your-pwa-file');
-  const fileBlob = await response.blob();
-
-  // Crea una respuesta personalizada con el archivo para descargar
-  const headers = { 'Content-Disposition': 'attachment; filename="pwa-file.extension"' };
-  const customizedResponse = new Response(fileBlob, { headers });
-
-  return customizedResponse;
-}
